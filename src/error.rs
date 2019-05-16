6c9d47e02d48819e7a8f64cbbbf2051058254ce1
Chris Bouchard <chris@upliftinglemma.net>
2019-05-15 20:12:02 -0400

    Add framework for Error types
    We'll have a generic Error type, which contains a "kind" and an optional
    boxed source error.

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,73 @@
-use std::{fmt, io, num};
+use std::{error, fmt};
 
-error_chain! {
-    foreign_links {
-        Fmt(fmt::Error);
-        Io(io::Error);
-        TryFromInt(num::TryFromIntError);
+pub trait ErrorKind: fmt::Debug + fmt::Display {}
+
+#[derive(Debug, Display)]
+#[display(fmt = "{}", kind)]
+pub struct Error<K>
+where
+    K: ErrorKind,
+{
+    pub kind: K,
+    source: Option<Box<dyn error::Error + Send + Sync + 'static>>,
+}
+
+impl<K> error::Error for Error<K>
+where
+    K: ErrorKind,
+{
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        self.source.as_ref().map(|boxed| {
+            let source: &(dyn error::Error + 'static) = boxed.as_ref();
+            source
+        })
+    }
+}
+
+impl<K> From<K> for Error<K>
+where
+    K: ErrorKind,
+{
+    fn from(kind: K) -> Self {
+        Error { kind, source: None }
+    }
+}
+
+pub trait ErrorExt {
+    fn wrap_as_source_of<K>(self, kind: K) -> Error<K>
+    where
+        K: ErrorKind;
+}
+
+impl<E> ErrorExt for E
+where
+    E: error::Error + Send + Sync + 'static,
+{
+    fn wrap_as_source_of<K>(self, kind: K) -> Error<K>
+    where
+        K: ErrorKind,
+    {
+        Error {
+            kind,
+            source: Some(Box::new(self)),
+        }
+    }
+}
+
+pub trait ResultExt<T> {
+    fn map_err_as_source_of<K>(self, kind: K) -> Result<T, Error<K>>
+    where
+        K: ErrorKind;
+}
+
+impl<T, E> ResultExt<T> for Result<T, E>
+where
+    E: ErrorExt,
+{
+    fn map_err_as_source_of<K>(self, kind: K) -> Result<T, Error<K>>
+    where
+        K: ErrorKind,
+    {
+        self.map_err(|e| e.wrap_as_source_of(kind))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,9 @@
     unused_results
 )]
 #![feature(const_fn)]
-// `error_chain!` can recurse deeply
-#![recursion_limit = "1024"]
 
 #[macro_use]
-extern crate error_chain;
+extern crate derive_more;
 #[macro_use]
 extern crate log;
 
