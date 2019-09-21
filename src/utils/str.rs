1198635ecd54c9624aa53551753ecbea24cecee0
Chris Bouchard <chris@upliftinglemma.net>
2019-09-20 23:01:56 -0400

    Remove redundant impls

--- src/utils/str.rs
+++ src/utils/str.rs
@@ -4,19 +4,7 @@ pub trait CharacterLength {
     fn char_len(&self) -> usize;
 }
 
-impl CharacterLength for String {
-    fn char_len(&self) -> usize {
-        self.chars().count()
-    }
-}
-
 impl CharacterLength for str {
-    fn char_len(&self) -> usize {
-        self.chars().count()
-    }
-}
-
-impl<'a> CharacterLength for &'a str {
     fn char_len(&self) -> usize {
         self.chars().count()
     }
