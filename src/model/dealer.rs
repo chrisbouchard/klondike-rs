7a677b35f2ace2aa80228daf4f45d35d24d4b53e
Chris Bouchard <chris@upliftinglemma.net>
2019-11-08 20:24:22 -0500

    Rewrite dealer to deal in proper order
    Gotta go across, then down to the next level:
    
         1  2  3  4  5  6  7
            8  9 10 11 12 13
              14 15 16 17 18
                 19 20 21 22
                    23 24 25
                       26 27
                          28
    
    starting from the top of the deck (end of the deck vector).

--- src/model/dealer.rs
+++ src/model/dealer.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+use num_traits::ToPrimitive;
 use rand::{seq::SliceRandom, thread_rng};
 
 use super::{area, area_list, settings, Card, Game, Rank, Suit};
@@ -29,12 +30,12 @@ impl Dealer for AutoWinDealer {
         let stock = area::stock::UnselectedStock::create(vec![], settings);
         let talon = area::talon::UnselectedTalon::create(vec![], 0);
 
-        let mut tableaux = settings
+        let mut tableaux_areas = settings
             .tableaux_indices()
             .map(|index| area::tableaux::UnselectedTableaux::create(index, 0, vec![]))
             .collect::<Vec<_>>();
 
-        let mut foundation = Suit::values()
+        let mut foundation_areas = Suit::values()
             .map(|suit| {
                 let cards = Rank::values()
                     .map(|rank| Card { suit, rank })
@@ -44,8 +45,8 @@ impl Dealer for AutoWinDealer {
             .collect::<Vec<_>>();
 
         let mut areas: Vec<Box<dyn area::UnselectedArea>> = vec![stock, talon];
-        areas.append(&mut foundation);
-        areas.append(&mut tableaux);
+        areas.append(&mut foundation_areas);
+        areas.append(&mut tableaux_areas);
 
         let areas = area_list::AreaList::new(areas).expect("Unable to create AreaList");
         Game::new(areas)
@@ -67,24 +68,50 @@ where
     fn deal_game(&self, settings: &settings::GameSettings) -> Game {
         let mut deck = S::create_deck();
 
-        let mut tableaux = settings
-            .tableaux_indices()
-            .map(|index| {
-                let cards = deck.split_off_bounded(usize::from(index) + 1);
-                area::tableaux::UnselectedTableaux::create(index, 1, cards)
-            })
-            .collect::<Vec<_>>();
+        let mut tableaux_areas = {
+            let len: usize = settings.tableaux_len.into();
+
+            // Sum of 1 + 2 + ... + tableaux_len
+            let card_count = len * (len + 1) / 2;
+
+            let cards = deck.split_off_bounded(card_count);
+
+            let mut piles: Vec<Vec<Card>> = vec![vec![]; len];
+            let mut level = 0;
+            let mut index = 0;
+
+            for card in cards.into_iter().rev() {
+                piles[index].push(card);
+
+                if index < len - 1 {
+                    index += 1;
+                } else {
+                    level += 1;
+                    index = level;
+                }
+            }
+
+            piles
+                .into_iter()
+                .enumerate()
+                .map(|(index, cards)| {
+                    // TODO: Use a proper Result type instead of unwrap.
+                    let index = index.to_u8().unwrap();
+                    area::tableaux::UnselectedTableaux::create(index, 1, cards)
+                })
+                .collect::<Vec<_>>()
+        };
 
         let stock = area::stock::UnselectedStock::create(deck, settings);
         let talon = area::talon::UnselectedTalon::create(vec![], 0);
 
-        let mut foundation = Suit::values()
-            .map(|index| area::foundation::UnselectedFoundation::create(index, vec![], settings))
+        let mut foundation_areas = Suit::values()
+            .map(|suit| area::foundation::UnselectedFoundation::create(suit, vec![], settings))
             .collect::<Vec<_>>();
 
         let mut areas: Vec<Box<dyn area::UnselectedArea>> = vec![stock, talon];
-        areas.append(&mut foundation);
-        areas.append(&mut tableaux);
+        areas.append(&mut foundation_areas);
+        areas.append(&mut tableaux_areas);
 
         let areas = area_list::AreaList::new(areas).expect("Unable to create AreaList");
         Game::new(areas)
@@ -100,15 +127,9 @@ struct InOrderShuffle;
 
 impl Shuffle for InOrderShuffle {
     fn create_deck() -> Vec<Card> {
-        let mut cards = Vec::new();
-
-        for suit in Suit::values() {
-            for rank in Rank::values() {
-                cards.push(Card { rank, suit })
-            }
-        }
-
-        cards
+        Suit::values()
+            .flat_map(|suit| Rank::values().map(move |rank| Card { rank, suit }))
+            .collect::<Vec<_>>()
     }
 }
 
